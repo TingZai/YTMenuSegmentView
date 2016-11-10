0d45ab61c9a316cf54a26198821783de86d3f3a4
yuting <y_t209@163.com>
2016-11-10 22:19:49 +0800

    no message

---
+++ YTMenuSegementViewDemo/YTMenuSegementViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YTMenuSegementViewDemo
+//
+//  Created by 余婷 on 16/11/10.
+//  Copyright © 2016年 余婷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
